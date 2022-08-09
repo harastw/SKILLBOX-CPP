023aa344be761a600e5b9c99429a2d240d0fc52a
harastw <94912005+harastw@users.noreply.github.com>
2022-08-09 09:52:37 +0000

    Update main.cpp

--- 30/2/main.cpp
+++ 30/2/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
 #include <cpr/cpr.h>
 
@@ -9,5 +8,11 @@ using namespace std;
 int main()
 {
   cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/html"));
-  cout << r.text << "\n";
+  string str = r.text, new_str;
+  for (int i = str.find("<h1>") + 1; i < str.find("</h1>"); i++)
+  {
+    new_str.push_back(str[i]);
+  }
+  cout << new_str << "\n";
 }
+
