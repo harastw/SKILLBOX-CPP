06e824256b8296a8df007027b683f97bd5839c2a
harastw <94912005+harastw@users.noreply.github.com>
2022-09-16 14:58:24 +0700

    Update 2.cpp

--- 35/2.cpp
+++ 35/2.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
-#include <filesystem>
-namespace fs = std::filesystem;
+#include <vector>
+#include <memory>
+#include <unordered_set>
+#include <functional>
+typedef std::unordered_set<int, std::hash<int>,
+			   std::equal_to<int>> my_set;
 int main()
 {
-  [](fs::path somePath, const std::string ext)
-  {
-    for(auto& p: std::filesystem::recursive_directory_iterator(somePath)) {
-      if (!p.is_regular_file() || p.path().extension().compare(ext))
-	continue;
-      std::cout << p.path() << std::endl;
-    }
-  }("/home/ai/code/CPP/", ".cpp");
+  my_set s;
+  std::vector<int> v = { 3, 3, 3, 2, 3, 2, 4, 2, 7, 5, 5, };
+  [&s](std::vector<int> v) {
+    for (auto el : v)
+      s.insert(el);
+  }(v);
+  for (my_set::const_iterator it = s.begin(); it != s.end(); ++it)
+    std::cout << *it << " ";
+  std::cout << std::endl;
 }
+
 
