ef356545c2ec0f5efe5161e2171ba11aef102011
harastw <94912005+harastw@users.noreply.github.com>
2022-09-16 14:59:01 +0700

    Update 3.cpp

--- 35/3.cpp
+++ 35/3.cpp
@@ -1,49 +1,14 @@
 #include <iostream>
 #include <filesystem>
-int main() {
-  for(auto& p: std::filesystem::recursive_directory_iterator("/home/ai/code/CPP")) {
-    std::cout << p.path() << " - ";
-    if (p.is_regular_file()) std::cout << "file" << std::endl;
-    else std::cout << "dir" << std::endl;
-  }
+namespace fs = std::filesystem;
+int main()
+{
+  [](fs::path somePath, const std::string ext)
+  {
+    for(auto& p: std::filesystem::recursive_directory_iterator(somePath)) {
+      if (!p.is_regular_file() || p.path().extension().compare(ext))
+	continue;
+      std::cout << p.path() << std::endl;
+    }
+  }("/home/ai/code/CPP/", ".cpp");
 }
-
-/*
-  Задание 3 (к уроку 4)
-
-
-Цель задания
-
-Поработать с файловой системой, используя новую библиотеку std::filesystem.
-
-
-Что нужно сделать
-
-В нашем арсенале теперь есть возможность рекурсивно проходить по каталогам, то есть
-получить список всех файлов, каталогов (и подкаталогов), которые содержатся по указанному
-пути. Для этого в стандартной библиотеке есть
-std::filesystem::recursive_directory_iterator, принимающий на вход путь, который он будет
-обходить рекурсивно. На каждом шаге он будет выдавать очередной путь, это может быть как
-каталог, так и обычный файл. Например, код
-
-for(auto& p: fs::recursive_directory_iterator("D:\\dir1"))
-        std::cout << p.path() << '\n';
-
-выведет полные пути ко всем файлам и подкаталогам в директории D:\dir1. Вызов p.path()
-возвращает объект типа std::filesystem::path, который позволяет:
-
-    Определить, файл это или каталог при помощи метода is_regular_file().
-    Получить расширение для обычного файла при помощи метода extension().
-
-При помощи метода compare() можно сравнить расширение с произвольной строкой:
-p.path().extension().compare(“.torrent”).
-
-
-  Создайте лямбда-функцию, которая принимает на вход путь в файловой системе и искомое
-  расширение в виде строки, а возвращает список имён файлов, которые имеют данное
-  расширение(). Объявление нашей функции может начинаться так:
-
-auto recursiveGetFileNamesByExtension =
-       [](std::filesystem::path path,
-          const std::string extension)
-*/
